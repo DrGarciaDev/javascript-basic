18bcddd15c11fb14a9fdcd60902ed57220830268
Luis García <luis_grdz@hotmail.com>
2020-03-26 15:21:48 -0600

    Video 11: Variables

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-let nombre  = prompt('Cual es tu nombre');
-let edad    = prompt('Cual es tu edad');
+// let nombre  = prompt('Cual es tu nombre');
+// let edad    = prompt('Cual es tu edad');
 
-document.getElementById('app').innerHTML = `Bienvenido ${nombre} de ${edad}`;
+// document.getElementById('app').innerHTML = `Bienvenido ${nombre} de ${edad}`;
+
+let nombre, mensaje;
+
+nombre = "Juan";
+nombre = 'Juan';
+
+
+mensaje = 'Escapando \'comillas\'';
+
+mensaje = "Escapando " + "mensaje";
+
+let aprendiendo = 'aprendiendo', tecnologia = 'tecnologia';
+
+console.log(`${aprendiendo} ${tecnologia}`);
