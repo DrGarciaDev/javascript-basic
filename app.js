7e81162cb6b6acd2fb2033e0f0a5c23bb604bc1b
Luis García <luis_grdz@hotmail.com>
2020-04-10 22:52:58 -0500

    Video 18 y 19: convertir strings a numeros 1 y 2

--- app.js
+++ app.js
@@ -1,103 +1,113 @@
-// let nombre  = prompt('Cual es tu nombre');
-// let edad    = prompt('Cual es tu edad');
+// // let nombre  = prompt('Cual es tu nombre');
+// // let edad    = prompt('Cual es tu edad');
 
-// document.getElementById('app').innerHTML = `Bienvenido ${nombre} de ${edad}`;
+// // document.getElementById('app').innerHTML = `Bienvenido ${nombre} de ${edad}`;
 
-let nombre, mensaje;
+// let nombre, mensaje;
 
-nombre = "Juan";
-nombre = 'Juan';
+// nombre = "Juan";
+// nombre = 'Juan';
 
 
-mensaje = 'Escapando \'comillas\'';
+// mensaje = 'Escapando \'comillas\'';
 
-mensaje = "Escapando " + "mensaje";
+// mensaje = "Escapando " + "mensaje";
 
-let aprendiendo = 'aprendiendo java ', tecnologia = 'tecnologia';
+// let aprendiendo = 'aprendiendo java ', tecnologia = 'tecnologia';
 
-console.log(`${aprendiendo} ${tecnologia}`);
-console.log(`${aprendiendo.length}`);
-console.log(`${aprendiendo.concat(' tecno')}`);
-console.log(`${aprendiendo.toUpperCase()}`);
-console.log(`${aprendiendo.toLocaleLowerCase()}`);
-console.log(`${aprendiendo.indexOf('apren')}`);
-console.log(`${aprendiendo.substring(0, 2)}`);
-console.log(`${aprendiendo.slice(-2)}`);
-console.log(`${aprendiendo.slice(0,3)}`);
-console.log(`${aprendiendo.split(' ')}`);
-console.log(`${aprendiendo.replace('java', 'python')}`);
-console.log(`${aprendiendo.includes('java')}`);
-console.log(`${aprendiendo.repeat(2)}`);
+// console.log(`${aprendiendo} ${tecnologia}`);
+// console.log(`${aprendiendo.length}`);
+// console.log(`${aprendiendo.concat(' tecno')}`);
+// console.log(`${aprendiendo.toUpperCase()}`);
+// console.log(`${aprendiendo.toLocaleLowerCase()}`);
+// console.log(`${aprendiendo.indexOf('apren')}`);
+// console.log(`${aprendiendo.substring(0, 2)}`);
+// console.log(`${aprendiendo.slice(-2)}`);
+// console.log(`${aprendiendo.slice(0,3)}`);
+// console.log(`${aprendiendo.split(' ')}`);
+// console.log(`${aprendiendo.replace('java', 'python')}`);
+// console.log(`${aprendiendo.includes('java')}`);
+// console.log(`${aprendiendo.repeat(2)}`);
 
-// Numeros 
-const   n1 = 30,
-        n2 = 20,
-        n3 = 20.20,
-        n4 = .1020,
-        n5 = -3;
+// // Numeros 
+// const   n1 = 30,
+//         n2 = 20,
+//         n3 = 20.20,
+//         n4 = .1020,
+//         n5 = -3;
 
-let resultado;
+// let resultado;
 
-// suma 
-resultado = n1 + n2;
-// resta 
-resultado = n1 - n2;
-// multiplicacion 
-resultado = n1 * n2;
-// division 
-resultado = n1 / n2;
-// modulo 
-resultado = n1 % n2;
-// pi 
-resultado = Math.PI;
-// redondeos
-resultado = Math.round(2.5);
-resultado = Math.ceil(2.5);
-resultado = Math.floor(2.5);
-// raiz cuadrada 
-resultado = Math.sqrt(144);
-// numero absoluto 
-resultado = Math.abs(-144);
-// potencia 
-resultado = Math.pow(8, 3);
-// minimo 
-resultado = Math.min(8, 3, 4);
-// maximo 
-resultado = Math.max(8, 3, 4);
-// random 
-resultado = Math.random();
-// jerarquia de operaciones 
-resultado = 1 + 2 * 2;
+// // suma 
+// resultado = n1 + n2;
+// // resta 
+// resultado = n1 - n2;
+// // multiplicacion 
+// resultado = n1 * n2;
+// // division 
+// resultado = n1 / n2;
+// // modulo 
+// resultado = n1 % n2;
+// // pi 
+// resultado = Math.PI;
+// // redondeos
+// resultado = Math.round(2.5);
+// resultado = Math.ceil(2.5);
+// resultado = Math.floor(2.5);
+// // raiz cuadrada 
+// resultado = Math.sqrt(144);
+// // numero absoluto 
+// resultado = Math.abs(-144);
+// // potencia 
+// resultado = Math.pow(8, 3);
+// // minimo 
+// resultado = Math.min(8, 3, 4);
+// // maximo 
+// resultado = Math.max(8, 3, 4);
+// // random 
+// resultado = Math.random();
+// // jerarquia de operaciones 
+// resultado = 1 + 2 * 2;
 
-console.log(resultado);
-// tipos de datos 
-let valor;
+// console.log(resultado);
+// // tipos de datos 
+// let valor;
 
-valor = "Luis";
-valor = 0;
-valor = true;
-valor = null;
-valor = Symbol('simbolo');
-valor = [1,2,3];
-valor = {nombre: 'luis', apellido: 'garcia'};
-valor = new Date();
+// valor = "Luis";
+// valor = 0;
+// valor = true;
+// valor = null;
+// valor = Symbol('simbolo');
+// valor = [1,2,3];
+// valor = {nombre: 'luis', apellido: 'garcia'};
+// valor = new Date();
 
-console.log(typeof(valor));
+// console.log(typeof(valor));
 
-// operadores de comparacion 
-const num1 = 20;
-const num2 = 50;
-const num3 = '20';
+// // operadores de comparacion 
+// const num1 = 20;
+// const num2 = 50;
+// const num3 = '20';
 
-console.log( num1 > num2);
-console.log( num1 < num2);
+// console.log( num1 > num2);
+// console.log( num1 < num2);
 
-// comparador igual 
-console.log( num1 == num2);
-// compara hasta el tipo de dato 
-console.log( num1 === num2);
+// // comparador igual 
+// console.log( num1 == num2);
+// // compara hasta el tipo de dato 
+// console.log( num1 === num2);
 
-// diferentes 
-console.log( num1 != num2);
-console.log( num1 !== num2);
+// // diferentes 
+// console.log( num1 != num2);
+// console.log( num1 !== num2);
 
+// convertir strings a numeros 
+
+const num1 = '50';
+const num2 = 10;
+const num3 = 'tres';
+
+console.log( Number(num1) + num2);
+console.log( parseInt(num1) + num2);
+console.log( parseFloat(num1) + num2);
+console.log( parseFloat(num1).toFixed(1) + num2);
